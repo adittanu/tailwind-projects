9e0d4b9e577de6217e5aea06d3c14f3991c22ca8
Adit Tanu <adiettanu27@gmail.com>
2022-11-28 21:39:43 +0700

    [Login Modal] add: project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: [],
   theme: {
+    // fontFamily: {
+    //     sans: ['Mulish', 'sans-serf'],
+    //     mono: ['Rokkitt', 'monospace']
+    // },
     extend: {},
   },
   plugins: [],
